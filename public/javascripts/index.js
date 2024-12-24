ae552975050adfeebe70fc364d9d0088afbe31aa
Loïc DURAND <loicdurand.ld@gmail.com>
2024-12-23 21:29:16 -0400

    ajout Nedb

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,73 +1,86 @@
+import '@seald-io/nedb/browser-version/out/nedb';
+
 import App from 'unviewed';
 import Path from './lib/Path';
 
+
 const // 
+  db = {
+    projects: new Nedb({ filename: 'projects.db', autoload: true }),
+    controllers: new Nedb({ filename: 'controllers.db', autoload: true }),
+    actions: new Nedb({ filename: 'actions.db', autoload: true })
+  },
   head_title = document.querySelector('title'),
   PROJECT_NAME = head_title.innerText,
   { pathname, search } = location,
 
-  current_project = new Path(pathname).getProject(),
+  current_project = new Path(pathname).getProject();
 
-  title = new App({
-    container: head_title,
-    current_project,
-    events: {
-      onupdate: () => ({
-        'title': ({ target, current_project }) => {
-          if (current_project)
-            target.innerText = `${current_project}@${PROJECT_NAME}`;
+// RECH. PROJECT_EXISTS
+db.projects.findOne({ name: current_project }, (err, project) => {
+
+  if (project === null)
+    db.projects.insert({ name: current_project });
+
+  // RECH. PROJECTS
+  db.projects.find({}, (err, projects) => {
+
+    console.log({ projects });
+
+    const // 
+      title = new App({
+        container: head_title,
+        current_project,
+        events: {
+          onupdate: () => ({
+            'title': ({ target, current_project }) => {
+              if (current_project)
+                target.innerText = `${current_project}@${PROJECT_NAME}`;
+            }
+          })
         }
-      })
-    }
-  }),
+      }),
 
-  create_project_li = ({ project, active }) => new App({
+      create_project_li = ({ project, active }) => new App({
 
-    container: document.querySelector('#project-template'),
+        container: document.querySelector('#project-template'),
 
-    project,
-    active,
+        project,
+        active,
 
-    events: {
-      onupdate: () => ({
-        '.project-name': ({ target, project, active }) => {
-          target.innerHTML = project;
-          target.classList[active ? 'add' : 'remove']('active');
+        events: {
+          onupdate: () => ({
+            '.project-name': ({ target, project, active }) => {
+              target.innerHTML = project;
+              target.classList[active ? 'add' : 'remove']('active');
+            }
+          })
         }
-      })
-    }
-  }),
+      }),
 
-  projects_ul = new App({
+      projects_ul = new App({
 
-    container: '#projects-container',
+        container: '#projects-container',
 
-    projects: [current_project],
-    active_project: current_project,
+        projects,
+        active_project: current_project,
 
-    events: {
-      onupdate: () => ({
-        '#projects-list': ({ target, projects, active }) => {
-          target.innerHTML = '';
-          projects.forEach(project => {
-            const project_li = create_project_li({ project, active: current_project === project });
-            target.append(project_li.container);
-          });
+        events: {
+          onupdate: () => ({
+            '#projects-list': ({ target, projects, active }) => {
+              target.innerHTML = '';
+              projects.forEach(project => {
+                const project_li = create_project_li({ project: project.name, active: current_project === project.name });
+                target.append(project_li.container);
+              });
+            }
+          })
         }
-      })
-    }
 
-  });
+      });
 
-// window.proxy = new Proxy(state, {
-//   set(target, prop, receiver) {
-//     if (prop === 'current_project') {
-//       target[prop] = receiver;
-//       title.state.current_project = receiver;
-//       projects_ul.state.active = receiver;
-//       projects_ul.state.projects.push(project_li({ project: receiver }))
+  })
+  // FIN RECH. PROJECTS
 
-//       App.update(title, projects_ul);
-//     }
-//   }
-// });
+});
+// FIN RECH. PROJECT_EXISTS
